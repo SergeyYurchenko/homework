83d9ab531a51281b4051ab03a55766065bc1e6ac
Kudder <45853973+Kudder@users.noreply.github.com>
2019-03-20 16:57:36 +0200

    array copying

--- lesson6/app.js
+++ lesson6/app.js
@@ -4,20 +4,37 @@ function copy(object) {
 	let objectCopy = {};
      
      	for (let key in object) {
-
+     		
  			if (typeof object[key] !== "object") {
 	    		objectCopy[key] = object[key];
 	    	} 
-	    	
+
+	    	else if (typeof object[key] == "array") {
+	    		objectCopy = [];
+
+				for (let i = 0, len = obj.length; i < len; i++) {
+					arrayCopy[i] = copy(object[i]);
+				}
+	    	}
+
 	    	else {
-		    	objectCopy[key] = copy(obj[key]);
+		    	objectCopy[key] = copy(object[key]);
 			}	
 		}		
 	return objectCopy;
 }
 
-const obj = {name: 'Alex', age: 33, adress: {country: 'UA', city: 'Dnipro'}};
+const obj = {
+	name: 'Alex', 
+	age: 33, 
+	adress: {country: 'UA', city: 'Dnipro'},
+	langs: [
+        { name: 'English', level: 'c2'},
+        { name: 'Deutsch', level: 'b1'},
+        { name: 'Arabic',  level: 'a2'}
+    ]
+};
 const objCopy = copy(obj);
 
-console.log(obj, 'original');
+console.log(obj);
 console.log(objCopy);
