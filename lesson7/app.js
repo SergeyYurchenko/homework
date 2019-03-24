f61d107499472f28c660a21e3b391fa6d95199bb
Kudder <45853973+Kudder@users.noreply.github.com>
2019-03-24 11:08:51 +0200

    fixed

--- lesson7/app.js
+++ lesson7/app.js
@@ -1,17 +1,16 @@
 "use strict"
 
-function userName(name) {
-    
-    name = prompt('Как вас зовут?', '');
+function askUserName() {
+    const name = prompt('Как вас зовут?', '');
 
-	if (isNameFalse(name)) {
+	if (isNameWrong(name)) {
  		alert('Ошибка!');
- 		return userName(name);
+ 		return askUserName();
     } 
     return name;
 }
 
-function isNameFalse(name) {
+function isNameWrong(name) {
 	return !isNaN(name) || name === null || deleteSpace(name) === '';
 }
 
@@ -19,34 +18,42 @@ function deleteSpace(name){
     return (name.split(' ').join(''));     
 }
 
-document.getElementsByTagName('h1')[0].innerHTML = 'Привет, ' + userName(name);
+document.getElementsByTagName('h1')[0].innerHTML = 'Привет, ' + askUserName();
 
-function getNumber(number) {
-    let userNumberUl = document.createElement('ul');
-    document.body.appendChild(userNumberUl);
+function getNumber() {
+    const number = prompt('Введите число от 0 до 100', '');
 
-	if (isNumberFalse(number)) {
+	if (isNumberWrong(number)) {
         alert('Ошибка!');
-        return getNumber(prompt('Введите число от 0 до 100', ''));
+        return getNumber();
     } 
     else if (number < 0 || number > 100) {
        alert('Я сказал от 0 до 100!');
-       return getNumber(prompt('Введите число от 0 до 100', ''));
+       return getNumber();
     } 
-    else {
-        for (let i = 0; i < number; i++) {
-            let userNumberLi = document.createElement('li');
-            userNumberUl.appendChild(userNumberLi);
-            userNumberLi.innerHTML = i;
-        } 
-    }
+    return number;
 }
 
-function isNumberFalse(number) {
+function isNumberWrong(number) {
 	return isNaN(number) || number === null || deleteSpace(number) === '';
 }
 
-getNumber(prompt('Введите число от 0 до 100', ''));
+const number = getNumber();
+
+let userNumberUl = document.createElement('ul');
+document.body.appendChild(userNumberUl);
+
+for (let i = 0; i < number; i++) {
+    let userNumberLi = document.createElement('li');
+    userNumberUl.appendChild(userNumberLi);
+    userNumberLi.innerHTML = i;
+}
+
+
+
+
+
+
 
 
 
